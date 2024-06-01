d3660bc218f2016722c718d831cc767e642b6f6b
gshine <gshine@m.scnu.edu.cn>
2024-06-01 23:41:05 +0800

    Implement q_reverseK and q_sort
    Used sort algorithms include bubble sort, insertion sort, selection
    sort and merge sort.

--- queue.c
+++ queue.c
@@ -154,6 +154,8 @@ bool q_delete_dup(struct list_head *head)
 void q_swap(struct list_head *head)
 {
     // https://leetcode.com/problems/swap-nodes-in-pairs/
+    if (!head)
+        return;
     struct list_head *node, *safe, *prev, *next;
     list_for_each_safe (node, safe, head) {
         if (safe == head)
@@ -192,10 +194,227 @@ void q_reverse(struct list_head *head)
 void q_reverseK(struct list_head *head, int k)
 {
     // https://leetcode.com/problems/reverse-nodes-in-k-group/
+    if (!head)
+        return;
+    struct list_head *node, *safe, *prev, *next;
+    list_for_each_safe (node, safe, head) {
+        // get prev and next nodes around K nodes
+        prev = node->prev;
+        next = node;
+        int cnt = 0;
+        while (cnt < k && next != head) {
+            cnt++;
+            next = next->next;
+        }
+        if (cnt < k)
+            break;
+        safe = next->prev;
+
+        // reverse K nodes
+        struct list_head *p = node, *q;
+        while (p != next) {
+            q = p->next;
+            p->next = p->prev;
+            p->prev = q;
+
+            p = q;
+        }
+
+        // setup node, safe, prev, next
+        node->next = next;
+        next->prev = node;
+        safe->prev = prev;
+        prev->next = safe;
+
+        safe = next;
+    }
+}
+
+/* Bubble sort */
+/*
+static void q_bubble_sort(struct list_head *head, bool descend)
+{
+    if (!head)
+        return;
+
+    bool swapped = true;
+    struct list_head *node, *safe, *prev, *next;
+
+    while (swapped) {
+        swapped = false;
+        list_for_each_safe (node, safe, head) {
+            if (safe == head)
+                break;
+
+            element_t *e_node = list_entry(node, element_t, list);
+            element_t *e_safe = list_entry(safe, element_t, list);
+
+            int cmp = strcmp(e_node->value, e_safe->value);
+            if ((descend && cmp < 0) || (!descend && cmp > 0)) {
+                swapped = true;
+
+                // swap
+                prev = node->prev;
+                next = safe->next;
+                node->prev = safe;
+                safe->next = node;
+
+                node->next = next;
+                safe->prev = prev;
+                prev->next = safe;
+                next->prev = node;
+
+                // set next node
+                safe = node;
+            }
+        }
+    }
+}
+*/
+
+/* Insertion sort */
+/*
+static void q_insertion_sort(struct list_head *head, bool descend)
+{
+    if (!head)
+        return;
+
+    struct list_head *node, *safe;
+    list_for_each_safe (node, safe, head) {
+        struct list_head *prev = node->prev, *next;
+        // one node is already sorted
+        if (prev == head)
+            continue;
+
+        // remove
+        list_del(node);
+        element_t *e_node = list_entry(node, element_t, list);
+        element_t *e_prev = list_entry(prev, element_t, list);
+
+        // find position
+        int cmp = strcmp(e_prev->value, e_node->value);
+        while ((descend && cmp < 0) || (!descend && cmp > 0)) {
+            prev = prev->prev;
+            if (prev == head)
+                break;
+            e_prev = list_entry(prev, element_t, list);
+            cmp = strcmp(e_prev->value, e_node->value);
+        }
+
+        // insertion
+        next = prev->next;
+        prev->next = node;
+        node->prev = prev;
+        node->next = next;
+        next->prev = node;
+    }
+}
+*/
+
+/* Selection sort */
+/*
+static void q_selection_sort(struct list_head *head, bool descend)
+{
+    if (!head)
+        return;
+
+    struct list_head *node, *safe, *prev = head;
+    list_for_each_safe (node, safe, head) {
+        struct list_head *temp = node->next, *sele = node;
+
+        // selection
+        while (temp != head) {
+            element_t *e_sele = list_entry(sele, element_t, list);
+            element_t *e_temp = list_entry(temp, element_t, list);
+
+            int cmp = strcmp(e_sele->value, e_temp->value);
+            if ((descend && cmp < 0) || (!descend && cmp > 0)) {
+                sele = temp;
+            }
+            temp = temp->next;
+        }
+
+        // insertion
+        list_del(sele);
+        prev->next->prev = sele;
+        sele->next = prev->next;
+        prev->next = sele;
+        sele->prev = prev;
+
+        // set next node
+        prev = sele;
+        safe = sele->next;
+    }
+}
+*/
+
+/* Merge two linked list */
+static void merge(struct list_head **l1,
+                  struct list_head **const l2,
+                  bool descend)
+{
+    struct list_head **temp = l1;
+    struct list_head *node1 = *l1;
+    struct list_head *node2 = *l2;
+
+    while (node1 && node2) {
+        element_t *elem1 = list_entry(node1, element_t, list);
+        element_t *elem2 = list_entry(node2, element_t, list);
+
+        int cmp = strcmp(elem1->value, elem2->value);
+        if ((descend && cmp < 0) || (!descend && cmp > 0)) {
+            *temp = node2;
+            node2 = node2->next;
+        } else {
+            *temp = node1;
+            node1 = node1->next;
+        }
+        temp = &(*temp)->next;
+    }
+
+    *temp = node1 ? node1 : node2;
+}
+
+/* Merge sort */
+static void q_merge_sort(struct list_head **head, bool descend)
+{
+    if (!(*head) || !(*head)->next)
+        return;
+
+    // get the middle node by fast and slow pointers
+    struct list_head *p = *head;
+    struct list_head *q = (*head)->next;
+    while (q && q->next) {
+        p = p->next;
+        q = q->next->next;
+    }
+
+    // set an additional list head
+    struct list_head *l2 = p->next;
+    p->next = NULL;
+
+    q_merge_sort(head, descend);
+    q_merge_sort(&l2, descend);
+    merge(head, &l2, descend);
 }
 
 /* Sort elements of queue in ascending/descending order */
-void q_sort(struct list_head *head, bool descend) {}
+void q_sort(struct list_head *head, bool descend)
+{
+    // q_bubble_sort(head, descend);
+    // q_insertion_sort(head, descend);
+    // q_selection_sort(head, descend);
+
+    head->prev->next = NULL;
+    q_merge_sort(&head->next, descend);
+    struct list_head *node, *prev = head;
+    for (node = head->next; node; node = node->next) {
+        node->prev = prev;
+        prev = node;
+    }
+    prev->next = head;
+    head->prev = prev;
+}
 
 /* Remove every node which has a node with a strictly less value anywhere to
  * the right side of it */
@@ -205,16 +424,16 @@ int q_ascend(struct list_head *head)
     return 0;
 }
 
-/* Remove every node which has a node with a strictly greater value anywhere to
- * the right side of it */
+/* Remove every node which has a node with a strictly greater value anywhere
+ * to the right side of it */
 int q_descend(struct list_head *head)
 {
     // https://leetcode.com/problems/remove-nodes-from-linked-list/
     return 0;
 }
 
-/* Merge all the queues into one sorted queue, which is in ascending/descending
- * order */
+/* Merge all the queues into one sorted queue, which is in
+ * ascending/descending order */
 int q_merge(struct list_head *head, bool descend)
 {
     // https://leetcode.com/problems/merge-k-sorted-lists/
